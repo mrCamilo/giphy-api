2559e12f79c79879e42badde8124845a4c75aaca
Manuel Camilo <mcamilo@protonmail.com>
2019-10-07 19:51:40 -0400

    .

--- app.js
+++ app.js
@@ -9,4 +9,22 @@ function displayRapGifs() {
     var rapper = $(this).attr("data-name");
     var queryURL = "http://api.giphy.com/v1/gifs/search?api_key=" + apiKey + "&q=" + movie;
     console.log(queryURL);
+
+    // Creating an AJAX call for the specific movie button being clicked
+    $.ajax({
+        url: queryURL,
+        method: "GET"
+      }).then(function(response) {
+
+        // Creating a div to hold the movie
+        var rapperDiv = $("<div class='rapper'>");
+
+        // Store rating data
+        var rating = response.Rated;
+
+        // Creating an element to have the rating displayed
+        var giphyRating = $("<p>").text("Rating: " + rating);
+
+        // Displaying the rating
+        movieDiv.append(giphyRating);
 }
