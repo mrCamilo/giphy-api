54de74f4ccb58dcafaa8317813a5025e6bddcd08
Manuel Camilo <mcamilo@protonmail.com>
2019-10-07 19:48:35 -0400

    .

--- app.js
+++ app.js
@@ -1,7 +1,12 @@
+// api key
+var apiKey = "g78LI21YarNhjYhB3RHzhmtgevtDoTbS";
+
 // Rappers array
 var rapArray = ["Nas", "Wu-Tang Clan", "Ice Cube", "MF Doom", "Earl Sweatshirt", "DMX", "2pac", "Kool G Rap"];
 
 // Function to display hip hop gifs
 function displayRapGifs() {
-    var rapper = $(this).attr("data-name"); 
+    var rapper = $(this).attr("data-name");
+    var queryURL = "http://api.giphy.com/v1/gifs/search?api_key=" + apiKey + "&q=" + movie;
+    console.log(queryURL);
 }
