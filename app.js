a532439d136493afe3e5242fae61db53df5e51a8
Manuel Camilo <mcamilo@protonmail.com>
2019-10-07 19:41:36 -0400

    .

--- app.js
+++ app.js
@@ -2,3 +2,6 @@
 var rapArray = ["Nas", "Wu-Tang Clan", "Ice Cube", "MF Doom", "Earl Sweatshirt", "DMX", "2pac", "Kool G Rap"];
 
 // Function to display hip hop gifs
+function displayRapGifs() {
+    var rapper = $(this).attr("data-name"); 
+}
