25b636946ba899b6a967a042f3bce32de58f9ed5
Manuel Camilo <mcamilo@protonmail.com>
2019-10-07 22:13:39 -0400

    .

--- app.js
+++ app.js
@@ -2,12 +2,12 @@
 var apiKey = "g78LI21YarNhjYhB3RHzhmtgevtDoTbS";
 
 // Rappers array
-var rapArray = ["Nas", "Wu-Tang Clan", "Ice Cube", "MF Doom", "Earl Sweatshirt", "DMX", "2pac", "Kool G Rap"];
+var rapArray = ["2pac", "Nas", "Madlib", "MF Doom", "Earl Sweatshirt", "DMX", "Wu-Tang Clan", "Kool G Rap"];
 
 // Function to display hip hop gifs
 function displayRapStuff() {
-    var rapper = $(this).attr("rapName");
-    var queryURL = "http://api.giphy.com/v1/gifs/search?api_key=" + apiKey + "&q=" + rapper;
+    var rapper = $(this).attr("rap-name");
+    var queryURL = "http://api.giphy.com/v1/gifs/search?api_key=" + apiKey + "&q=" + rapper + "&limit=10";
 
     // Creating an AJAX call for the specific movie button being clicked
     $.ajax({
@@ -15,18 +15,19 @@ function displayRapStuff() {
         method: "GET"
       }).then(function(giphyResponse) {
 
-        // Creating a div to hold the movie
+        // Creating a div to hold the artists
         var rapperDiv = $("<div class='rap'>");
 
         // $("body").append('<img src =' + giphyResponse.data[1].images.fixed_height.url + "/>")
 
-        // Retrieving the URL for the image
-        var imgURL = giphyResponse.data[3].images.fixed_height.url;
-
+        // Retrieving the URL for the images, using a for loop to get 10 images.
+        for (var i = 0; i < 10 ; i++) {
+        var imgURL = giphyResponse.data[i].images.fixed_height.url;
+        
         $("body").append('<img src =' + imgURL + "/>")
-
+        }
         // Creating an element to hold the image
-        var image = $("<img>").attr("src", imgURL);
+        //var image = $("<img>").attr("src", imgURL);
 
         // Append the image
         //rapperDiv.append(image);
@@ -43,7 +44,7 @@ function makeButtons() {
         // Adding a class of movie-btn to our button
         a.addClass("rap-btn");
         // Adding a data-attribute
-        a.attr("data-name", rapArray[i]);
+        a.attr("rap-name", rapArray[i]);
         // Providing the initial button text
         a.text(rapArray[i]);
         // Adding the button to the buttons-view div
