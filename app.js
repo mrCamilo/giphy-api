bc190055ba32db2b04b34f3de49a1a8bac493b70
Manuel Camilo <mcamilo@protonmail.com>
2019-10-07 21:23:01 -0400

    .

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ var apiKey = "g78LI21YarNhjYhB3RHzhmtgevtDoTbS";
 var rapArray = ["Nas", "Wu-Tang Clan", "Ice Cube", "MF Doom", "Earl Sweatshirt", "DMX", "2pac", "Kool G Rap"];
 
 // Function to display hip hop gifs
-function displayRapGifs() {
+function displayRapStuff() {
     var rapper = $(this).attr("data-name");
     var queryURL = "http://api.giphy.com/v1/gifs/search?api_key=" + apiKey + "&q=undertale";
 
@@ -22,3 +22,40 @@ function displayRapGifs() {
         $(".rapper").append('<img src =' + giphyResponse.data[3].images.fixed_width.url + "/>")
 }
 )}
+
+function makeButtons() {
+    $("#hipHopButtons").empty(); // empty buttons div so it doesn't repeat
+
+    for (var i = 0; i < rapArray.length; i++) {
+
+        // Then dynamicaly generating buttons for each movie in the array
+        var a = $("<button>"); // create a button
+        // Adding a class of movie-btn to our button
+        a.addClass("rap-btn");
+        // Adding a data-attribute
+        a.attr("data-name", rapArray[i]);
+        // Providing the initial button text
+        a.text(rapArray[i]);
+        // Adding the button to the buttons-view div
+        $("#hipHopButtons").append(a);
+      }
+}
+
+// This function handles events where a movie button is clicked
+    $("#add-movie").on("click", function(event) {
+        event.preventDefault();
+        // This line grabs the input from the textbox
+        var rapper = $("#rapperinput").val().trim();
+
+        // Adding movie from the textbox to our array
+        rappers.push(rapper);
+
+        // Calling renderButtons which handles the processing of our movie array
+        makeButtons();
+      });
+
+      // Adding a click event listener to all elements with a class of "movie-btn"
+      $(document).on("click", ".rap-btn", displayRapStuff);
+
+      // Calling the renderButtons function to display the intial buttons
+      makeButtons();
