b42b3b66b7d939911a81be0e587870ebf3aaeb71
Manuel Camilo <mcamilo@protonmail.com>
2019-10-07 21:37:54 -0400

    .

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ var rapArray = ["Nas", "Wu-Tang Clan", "Ice Cube", "MF Doom", "Earl Sweatshirt",
 // Function to display hip hop gifs
 function displayRapStuff() {
     var rapper = $(this).attr("data-name");
-    var queryURL = "http://api.giphy.com/v1/gifs/search?api_key=" + apiKey + "&q=undertale";
+    var queryURL = "http://api.giphy.com/v1/gifs/search?api_key=" + apiKey + "&q=" + "data-name";
 
     // Creating an AJAX call for the specific movie button being clicked
     $.ajax({
@@ -17,9 +17,17 @@ function displayRapStuff() {
         console.log(giphyResponse);
 
         // Creating a div to hold the movie
-        var rapperDiv = $("<div class='rapper'>");
+        var rapperDiv = $("<div class='rapDiv'>");
+        $(".rapperDiv").append('<img src =' + giphyResponse.data[3].images.fixed_width.url + "/>")
 
-        $(".rapper").append('<img src =' + giphyResponse.data[3].images.fixed_width.url + "/>")
+        // Retrieving the URL for the image
+        var imgURL = giphyResponse.data[3].images.fixed_height.url;
+
+        // Creating an element to hold the image
+        var image = $("<img>").attr("src", imgURL);
+
+        // Append the image
+        rapperDiv.append(image);
 }
 )}
 
@@ -48,7 +56,7 @@ function makeButtons() {
         var rapper = $("#rapperinput").val().trim();
 
         // Adding movie from the textbox to our array
-        rappers.push(rapper);
+        rapArray.push(rapper);
 
         // Calling renderButtons which handles the processing of our movie array
         makeButtons();
