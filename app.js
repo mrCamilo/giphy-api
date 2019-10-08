9250f53fc0bec047c0737f0c8f70a8ec932c49f5
Manuel Camilo <mcamilo@protonmail.com>
2019-10-07 21:52:50 -0400

    .

--- app.js
+++ app.js
@@ -6,22 +6,24 @@ var rapArray = ["Nas", "Wu-Tang Clan", "Ice Cube", "MF Doom", "Earl Sweatshirt",
 
 // Function to display hip hop gifs
 function displayRapStuff() {
-    var rapper = $(this).attr("data-name");
-    var queryURL = "http://api.giphy.com/v1/gifs/search?api_key=" + apiKey + "&q=" + "data-name";
+    var rapper = $(this).attr("rapName");
+    var queryURL = "http://api.giphy.com/v1/gifs/search?api_key=" + apiKey + "&q=" + "rapName";
 
     // Creating an AJAX call for the specific movie button being clicked
     $.ajax({
         url: queryURL,
         method: "GET"
       }).then(function(giphyResponse) {
-        console.log(giphyResponse);
 
         // Creating a div to hold the movie
-        var rapperDiv = $("<div class='rapDiv'>");
-        $(".rapperDiv").append('<img src =' + giphyResponse.data[3].images.fixed_width.url + "/>")
+        var rapperDiv = $("<div class='rap'>");
+
+        // $("body").append('<img src =' + giphyResponse.data[1].images.fixed_height.url + "/>")
 
         // Retrieving the URL for the image
         var imgURL = giphyResponse.data[3].images.fixed_height.url;
+
+        $("body").append('<img src =' + imgURL + "/>")
 
         // Creating an element to hold the image
         var image = $("<img>").attr("src", imgURL);
